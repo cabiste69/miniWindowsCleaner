3ed0821cc4930d830022798b257ea7f9c3524929
cabiste <ahmedjj354@gmail.com>
2022-05-15 01:05:07 +0100

    added more folders to the list

--- README.md
+++ README.md
@@ -1,47 +1,65 @@
 # miniWindowsCleaner
-this is a small script that cleans windows from useless files (temp folders)
-
-# how to use
-download the latest release from here https://github.com/cabiste69/miniWindowsCleaner/releases/latest
-
-then make a shortcut of the app and place it in the windows startup folder `C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp`
-
-that way it will run when the pc turns on
+This is a small script that cleans windows from useless files (temp folders)
 
 # intent 
-obviously this app sounds stupid "huh? it just deletes the temp folder? i can do that manually"
-well yeah but it will save you a bit of time so why not ¯\\\_(ツ)\_/¯
-the app is meant to run once one you turn on your machine, clean it then you never see it again
+Obviously this app sounds stupid, "huh? it just deletes the temp folders? i can do that manually"
+
+Well yeah that's correct but this app will save you a bit of time so why not ¯\\\_(ツ)\_/¯
+
+The app is meant to run once one you turn on your machine, clean it then you never see it again
+
+# how to use
+Download the latest release from here https://github.com/cabiste69/miniWindowsCleaner/releases/latest
+
+Then make a shortcut of the app and place it in the windows startup folder `C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp`
+
+That way it will run when the pc turns on
 
 # how to build
 
+1. Install the dependencies 
+   - `pip install winshell`
+   - `pip install pypiwin32`
+
+2. Run `python app.py`
+
+# how to Compile
+
 1. Install pyinstaller `pip install pyinstaller`
-2. compile the source code `pyinstaller -F app.py`
+2. Compile the source code `pyinstaller -F app.py`
    - `-F` so it compiles everything into 1 single file
 
-this should give you an exe that you'll find in `dist/app.exe`
-copy that app to another folder so you don't forget about it just in case
+This should give you an exe that you'll find in `dist/app.exe`,
+
+Copy that app to another folder so you don't forget about it just in case
 
 # known issues 
-none :)
+None :)
 
 # FAQ
-Q: does this just moves the files to the bin and i have to manually empty it?
+Q: Does this app just move the files to the recycle bin and i have to manually empty it?
 
-A: nope, it deletes it immediately :)
+A: Nope, it deletes them completely :)
 
-Q: is a virus?
+---
 
-A: no :(, you can read the code yourself in `app.py` literally 20 lines of code
+Q: Is it a virus?
 
-Q: what folders are deleted exactly?
+A: no :(, you can read the code yourself in `app.py` it's less than 100 lines of code
 
-A: as of right now it only deletes the normal temp folder (`C:\Users\comp\AppData\Local\Temp`), but i'll be adding these other folders in the near future
+---
+
+Q: What folders are deleted exactly?
+
+A: It deletes the windows temp folders that are known to be safe:
+   - C:\Users\comp\AppData\Local\Temp
    - C:\Windows\Temp
-   - C:\Windows.old
    - C:\Windows\Downloaded Program Files
    - C:\Windows\Prefetch
    - C:\Windows\Offline Web Pages
-   - recycle bin
+   - C:\Windows.old (disabled by default)
+   - recycle bin (disabled by default)
 
-  *I'll also make a config file where you can choose which folders to be deleted easily*
+  *The app will creat a config file in the folder you place it in*
+  *You can enable/disable the folders to be deleted by adding a `#` before them*
+  *You can add your own folders by just typing them in a new line*
--- app.py
+++ app.py
@@ -1,28 +1,93 @@
-import tempfile
 import os
 import shutil
+import tempfile
+from winshell import recycle_bin
 
-# get the temp folder's path
-temppath = tempfile.mkdtemp()
-temppath = os.path.dirname(temppath)
 
-print("-----Deleting files-----")
-# ... delete every file in the temp folder
-for root, dirs, files in os.walk(temppath):
-    for file in files:
+sp = os.sep
+emptyBin = False
+
+def CreateConfigFile(path):
+    print("creating config file...")
+    # get windows' folder's path
+    winpath = os.environ['WINDIR']
+    x = [
+            os.path.dirname(tempfile.mkdtemp()), # normal temporary folder (%temp%)
+            winpath + sp + "Temp", # windows specific temp folder
+            winpath + sp + "Downloaded Program Files",
+            winpath + sp + "Prefetch",
+            winpath + sp + "Offline Web Pages",
+            "# " + winpath + ".old", # older version of windows
+            "# recycleBin"
+        ]
+
+    with open(path,"w") as f:
+        for row in x:
+            f.write(row+"\n")
+
+def ReadConfigFile():
+    print("reading the config file")
+    
+    cfgPath = os.path.dirname(__file__) + sp + "config.cfg"
+    folders = []
+    
+    if not os.path.exists(cfgPath):
+        CreateConfigFile(cfgPath)
+        
+    with open(cfgPath) as file:
+        for line in file:
+            line = line.strip()
+            if not line.startswith("#"):
+                if line == "recycleBin":
+                    global emptyBin
+                    emptyBin = True
+                else:
+                    folders.append(line)
+    
+    return folders
+
+
+def DeleteTemps(folders):
+    
+    for folder in folders:
+        if not os.path.isdir(folder):
+            continue
+        
+        print("------deleting: "+ folder+"------")
+        
+        print("Deleting files")
+        # ... delete every file in the folder
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file == "desktop.ini":
+                    continue
+                
+                try:
+                    os.remove(os.path.join(root, file))
+                except Exception as e:
+                    print(str(e))
+
+        print("Deleting folders")
+        # ... delete every folder in the folder
+        for file in os.listdir(folder):
+            if os.path.isdir(folder + sp + file):
+                try:
+                    shutil.rmtree(folder + sp + file)
+                except Exception as e:
+                    print(str(e))
+    
+    # finally empty recycle bin if specified
+    if emptyBin:
+        print("trying to empty recycle bin")
         try:
-            os.remove(os.path.join(root, file))
+            recycle_bin().empty(confirm=False, show_progress=False, sound=False)
         except Exception as e:
             print(str(e))
 
-print("-----Deleting folders-----")
-# ... delete every folder in the temp folder
-for folder in os.listdir(temppath):
-    if os.path.isdir(temppath + os.sep +folder):
-        try:
-            shutil.rmtree(temppath + os.sep + folder)
-        except Exception as e:
-            print(str(e))
-
-# i have it so it delete files then folders individually because windows sometimes doesn't allow you
+# i have it so it deletes the files then then folders individually because windows sometimes doesn't allow you
 # to delete folders with files in them but if you delete those files individually you can then delete the folder (yes windows is damn weird)
+
+
+if __name__ == "__main__":
+    foldersToDelete = ReadConfigFile()
+    DeleteTemps(foldersToDelete)
